fb03ea8d73605dd3b23a7d5f8ae36012e234b1e0
张凯博 <1120203493@bit.edu.cn>
2023-11-14 16:24:53 +0800

    11.14

--- js/indexl.js
+++ js/indexl.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Hi 房捷|祝你|生日快乐|happy birthday|#countdown 3||');
+      S.UI.simulate('Hi 康康|祝你|生日快乐|happy birthday|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
